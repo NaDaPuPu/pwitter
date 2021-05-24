2167f906e435db92984c687c351372828af73c42
NaDaPuPu <jun19998@gmail.com>
2021-05-24 15:49:08 +0900

    isLoggedIn이 없을 경우(render를 하나 줄였다!)

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,13 @@ import { authService } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       // 로그인, 로그아웃, 앱 초기화 시에 발생
       if (user) {
         // 로그인 한 user가 존재 시(user가 존재할 경우)
-        setIsLoggedIn(true); // 로그인 여부 확인용 state isLoggedIn을 true로 설정
-        setUserObj(user); // userObj에 user 입력
-      } else {
-        setIsLoggedIn(false);
+        setUserObj(user); // userObj에 user 입력\
       }
       setInit(true); // 앱이 준비가 되었을 때, init state를 true로 설정
     });
@@ -22,7 +18,7 @@ function App() {
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+        <AppRouter isLoggedIn={userObj}} userObj={userObj} />
       ) : (
         "Initializing..."
       )}
